221190bffc31a364c15f481e5297e2650e336bfe
ramez <ramezhishamahmad@gmail.com>
2024-02-23 15:51:27 +0300

    applying two way binding on Player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event){
+    setPlayerName(event.target.value)
+  }
+
+  let showPlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    showPlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {showPlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
