5d97909edce0f8319ea7d48f7dc0ba31cc1e0b39
ramez <ramezhishamahmad@gmail.com>
2024-02-24 22:09:06 +0300

    added Game over Component and Modified App component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+    if(isEditing) {
+        onChangeName(symbol, playerName);
+    }
+    
   }
-  function handleChange(event){
-    setPlayerName(event.target.value)
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   let showPlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    showPlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    showPlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
-    <li className={isActive ? "active": undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {showPlayerName}
         <span className="player-symbol">{symbol}</span>
